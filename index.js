8bed12804d7f10752cfadb23fd12405872a5023b
unknown <Alausa-AbdulAzeez>
2022-02-19 01:29:01 -0800

    first commit 22

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
 const moviesRoute = require("./routes/movies");
 const listsRoute = require("./routes/lists");
-// const cors = require("cors");
+const cors = require("cors");
 
 const path = require("path");
 
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("Connected to the DB"))
   .catch((error) => console.log(error));
 
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
@@ -28,6 +28,10 @@ app.use("/api/users", usersRoute);
 app.use("/api/movies", moviesRoute);
 app.use("/api/lists", listsRoute);
 
-const PORT = 8800;
+const PORT = process.env.PORT || 8800;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("/client/build"));
+}
 
 app.listen(PORT, () => console.log("Backend server is running"));
